b4d0599716b974bf80b0166e32c3381e71f3efba
Dom <dominicralphs@msn.com>
2019-09-28 17:11:24 +0100

    Add project files.

---
+++ soft356/main.cpp
@@ -0,0 +1,18 @@
+//simple hello world program
+#include <iostream> 
+#include <conio.h>
+using namespace std;
+
+void main ()
+{
+	cout << "hello world\n";
+	cout << "press a key to close!!";
+	_getch();
+
+
+
+
+
+
+
+}
